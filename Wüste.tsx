f6421e48ab160aed2a29288125b1a83822bf8dee
Drachensegler <github@drachensegler.kloenk.de>
2021-01-23 18:55:19 +0100

    mv

---
+++ Wüste.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="Wüste " tilewidth="32" tileheight="32" spacing="1" tilecount="48" columns="8">
+ <image source="../Welt/examples/tmw_desert_spacing.png" width="265" height="199"/>
+</tileset>
---
+++ hexmini.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="hexmini" tilewidth="32" tileheight="32" tilecount="6" columns="3">
+ <image source="../Welt/examples/hexmini.png" width="106" height="72"/>
+</tileset>
