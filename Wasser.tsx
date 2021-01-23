b6ea6bec88caa166794f35c1b91bc153d1b56ac7
Drachensegler <github@drachensegler.kloenk.de>
2021-01-23 18:45:26 +0100

    init

---
+++ Wasser.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="wasser" tilewidth="32" tileheight="32" tilecount="3072" columns="64">
+ <image source="../office-map/water.png" trans="ff00ff" width="2048" height="1536"/>
+</tileset>
